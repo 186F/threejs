d13577bdd3aa8fedd8966d75ef6f38d185c0a4bc
186F <scottmh541@gmail.com>
2024-04-29 09:06:09 -0400

    05
    commit after completing 05 - animations. i've made a cube, and now that cube can move

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import gsap from 'gsap'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -53,4 +54,37 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+/* // Clock
+const clock = new THREE.Clock() */
+
+gsap.to(group.position, { 
+    duration: 1,
+    delay: 1,
+    x: 2
+})
+
+gsap.to(group.position, { 
+    duration: 1,
+    delay: 2,
+    x: 0
+})
+
+// Animations
+const tick = () =>
+{
+
+/*     // Clock
+    const elapsedTime = clock.getElapsedTime()
+    console.log(elapsedTime)
+
+    //Update objects
+    camera.position.y = Math.sin(elapsedTime)
+    group.position.x = Math.cos(elapsedTime) */
+
+    // Render
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick)
+}
+
+tick()
